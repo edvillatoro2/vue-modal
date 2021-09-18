e8a31e1037af7192fe23220c52af7a025b4d79d9
edvillatoro2 <34876232+edvillatoro2@users.noreply.github.com>
2021-09-18 08:59:53 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <main>
+    <h1>Modal</h1>
+    <button @click="() => toggleModal('buttonTrigger')">Open</button>
+    <Modal v-if="modalTrigger.buttonTrigger" 
+    :toggleModal="() => toggleModal('buttonTrigger')">
+      <h2>Text here</h2>
+      </Modal>
+  </main>
+</template>
+
+<script>
+import {ref} from 'vue';
+import Modal from './components/Modal.vue'
+
+export default {
+  setup() {
+    const modalTrigger = ref({
+      buttonTrigger: true,
+    });
+
+    const toggleModal = (trigger) => {
+      modalTrigger.value[trigger] = !modalTrigger.value[trigger]
+    }
+
+    return {
+      Modal,
+      modalTrigger,
+      toggleModal
+    }
+  },
+    components: {
+      Modal
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  padding: 50px;
+  h1 {
+    color: rgba($color: #686565b4, $alpha: 0.4);
+    font-size: 10rem;
+    text-shadow: 10px 0 0 black;
+  }
+  h2 {
+    color: rgba($color: #686565b4, $alpha: 0.4);
+    text-shadow: 4px 0 0 black;
+    font-size: 4rem;
+    letter-spacing: 4px;
+    // font-weight: bolder;
+    margin: 0px;
+  }
+    button {
+    min-width: 150px;
+    min-height: 50px;
+    background: white;
+    padding: 0.3rem 3.4rem;
+    border: 3px solid black;
+    margin-right: 2.6rem;
+    box-shadow: 0 0 0 black;
+    transition: all 0.2s;
+    cursor: pointer;
+    &:hover {
+      box-shadow: 0.4rem 0.4rem 0 black;
+      transform: translate(-0.4rem, -0.4rem);
+    }
+    &:active {
+        box-shadow: 0 0 0 black;
+        transform: translate(0, 0);
+    }
+  }
+}
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="modal">
+        <div class="inner">
+            <slot />
+            <button class="close" @click="toggleModal()">close</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['toggleModal']
+}
+</script>
+
+<style lang="scss" scoped>
+    .modal {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .inner {
+            background-color: white;
+            padding: 32px;
+            position: relative;
+            width: 50%;
+            top: 0;
+            text-align: center;
+            // border: 3px solid black;
+            box-shadow: 0.8rem 0.8rem rgba(59, 59, 59, 0.527), -0.8rem -0.8rem rgba(165, 165, 165, 0.589);
+            animation: modalbox .7s cubic-bezier(0,0,.3,1.6);
+            @keyframes modalbox {
+                0% {
+                    top: -350px; 
+                    opacity: 0;
+                    width: 20px;
+                }
+                100% {
+                    top: 0; 
+                    opacity: 1;
+                }
+            }
+            button {
+                min-width: 150px;
+                min-height: 50px;
+                background: white;
+                padding: 0.3rem 3.4rem;
+                border: 3px solid rgb(204, 14, 14);
+                margin-right: 2.6rem;
+                box-shadow: 0 0 0 white;
+                font-size: 1.2rem;
+                letter-spacing: -1.2px;
+                font-family: Helvetica, Arial, sans-serif; 
+                font-weight: bolder; 
+                font-style : italic;
+                transition: all 0.2s;
+                cursor: pointer;
+                &:hover {
+                    box-shadow: 0.4rem 0.4rem 0 rgb(204, 14, 14);
+                    transform: translate(-0.6rem, -0.6rem);
+                }
+                &:active {
+                    box-shadow: 0 0 0 rgb(204, 14, 14);
+                    transform: translate(0, 0);
+                }
+            }
+        }
+    }
+</style>
